03eff5ee33c3c820d32151794e740401144b6468
hchev001 <hchev001@gmail.com>
2016-12-20 08:17:12 -0500

    created button listener and exponent method

--- app/src/main/java/com/hamiltonchevez/pitothenth/MainActivity.java
+++ app/src/main/java/com/hamiltonchevez/pitothenth/MainActivity.java
@@ -2,6 +2,9 @@ package com.hamiltonchevez.pitothenth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calculate = (Button) findViewById(R.id.calculate);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText power = (EditText) findViewById(R.id.nthPowerField);
+                EditText result = (EditText) findViewById(R.id.resultField);
+                double answer = raisePiToTheNth(power);
+                result.setText(Double.toString(answer));
+
+            }
+        });
+
+
+    }
+
+    public double raisePiToTheNth(EditText exponent){
+        double exp = Double.parseDouble(exponent.getText().toString());
+        double expResult = Math.pow(Math.PI,exp);
+        return expResult;
     }
 }
